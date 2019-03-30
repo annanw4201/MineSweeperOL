9490424601d05385c9897655d3c1105d99e71cb4
Wong Tom <23562997+tomwang1024@users.noreply.github.com>
2019-03-30 12:25:44 -0400

    The phaser can now load the image (might don't need phaser for the game, but try for it)

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,6 +1,6 @@
 let config = {
     type: Phaser.AUTO,
-    parent: 'phaser-mineSweeper',
+    parent: 'game',
     width: 800,
     height: 600,
     scene: {
@@ -14,10 +14,30 @@ let game = new Phaser.Game(config);
 
 function preload() {
     console.log('preload');
+    this.load.tilemapTiledJSON('map', 'assets/map/tilesheet.json');
+    this.load.image('tiles', 'assets/map/tilesheet.png', { frameWidth: 100, frameHeight: 100 });
+    this.load.image('sky', 'assets/map/sky.png');
 }
 
 function create() {
     console.log('create');
+    this.add.image(400, 300, 'sky');
+    const map = this.make.tilemap({key: "map", tileWidth: 100, tileHeight: 100});
+    const tileSet = map.addTilesetImage("tilesheet", "tiles");
+    var layer;
+    for (let i = 0; i < map.layers.length; ++i) {
+        layer = map.createStaticLayer(i, tileSet, 100, 0);
+    }
+    
+    
+    socketSetup();
+}
+
+function update() {
+    
+}
+
+function socketSetup() {
     this.socket = io();
     this.socket.on('currentPlayer', function (players) {
         console.log(players);
@@ -31,8 +51,4 @@ function create() {
         console.log(player);
         $('#players_display').append('<li>' + player.name + '</i>');
     });
-}
-
-function update() {
-    
 }
